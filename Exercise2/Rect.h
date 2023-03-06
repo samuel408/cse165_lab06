3f56515dbbd7a3a457e587d37c0453b6e0167966
Samuel <samuel@Samuels-MacBook-Pro.local>
2023-03-05 21:55:58 -0800

    finished exercise 2

--- Exercise2/Rect.h
+++ Exercise2/Rect.h
@@ -1,13 +1,44 @@
 #ifndef RECT_H
 #define RECT_H
-
+#include <iostream>
+#include "Vec.h"
 using namespace std;
 
-class Rect{
-public:
-float a,b,c,d;
 
 
+class Rect : public Vec{
+    public:
+        float a, b, c, d;
+
+        Rect(){
+            a = 0.0f;
+            b = 0.0f;
+            c = 0.0f;
+            d = 0.0f;
+        }
+
+        Rect(float x, float y, float w, float h){
+            a = x;
+            b = y;
+            c = w;
+            d = h;
+        }
+       
+        bool contains(Vec vect){
+            float X = vect.x;
+            float Y = vect.y;
+            float xdis = a + c;
+            float ydis = b - d; 
+
+            if(X >= a && X <= xdis){
+                if(Y <= b && Y >= ydis){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
 };
-
-#endif 
+#endif
